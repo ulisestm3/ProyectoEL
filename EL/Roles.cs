8e7922d392662322d5f304dd69d042cfe3f0c783
Ulisestm3 <ulisestm3@gmail.com>
2023-06-21 19:43:29 -0600

    Actualizacion Sesion05 EntityFramework

--- EL/Roles.cs
+++ EL/Roles.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EL
 {
+    [Table("Roles")]
     public class Roles
     {
+        [Key]
         public short IdRol { get; set; }
+        
+        [MaxLength(50),Required]
         public string Rol { get; set; }
+
+        [Required]
         public bool Activo { get; set; }
+        [Required]
         public int IdUsuarioRegistra { get; set; }
+        [Required]
         public DateTime FechaRegistro { get; set; }
         public int? IdUsuarioActualiza { get; set; }
         public DateTime? FechaActualizacion { get; set; }
