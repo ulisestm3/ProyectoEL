74df06fbe9e9561e8df37459b712947aa3881542
ulisestm3 <ulisestm3@gmail.com>
2023-07-13 10:03:08 -0600

    Actualizando ServerManagerStudio - BD

--- DAL/Conexion.cs
+++ DAL/Conexion.cs
@@ -4,12 +4,15 @@ namespace DAL
 {
     public static class Conexion
     {
+        //Credenciales de la Base de Datos
         private static string NombreAplicacion = "GeneradorCapas";
-        private static string Servidor = @"LPT-ULISESZ\MSSQLSERVER01";
+        private static string Servidor = @"LPT-UZUNIGA";
         private static string Usuario = "";
         private static string Password = "";
         private static string BaseDatos = "MPOO";
 
+        //Scrip conexion string a la base de datos cuando es por autenticacion windows dejar en false
+        //Si es autenticacion SQL dejar en true
         public static string ConexionString(bool SqlAutentication = false)
         {
             SqlConnectionStringBuilder ConstructorCadenaConexion = new()
